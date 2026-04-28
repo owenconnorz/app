067ee27ac59f0f915b463a9db6aa256c77e15880
Owen <96526481+owenconnorz@users.noreply.github.com>
2026-04-28 21:12:26 +0100

    fixes

--- AioWebAndroid/settings.gradle.kts
+++ AioWebAndroid/settings.gradle.kts
@@ -11,6 +11,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://maven.pkg.github.com/TeamNewPipe/NewPipeExtractor") }
     }
 }
 
