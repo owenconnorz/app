f3fb6d65a60a2e7cc9f1171ca218af76e9fa9341
Owen <96526481+owenconnorz@users.noreply.github.com>
2026-04-28 23:12:08 +0100

    its killing me

--- AioWebAndroid/app/build.gradle.kts
+++ AioWebAndroid/app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17")
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17"
 }
 
 android {
