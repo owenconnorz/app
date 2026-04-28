fc7626791a9346c104c288ed6b40f99a30a1ddd1
Owen <96526481+owenconnorz@users.noreply.github.com>
2026-04-28 23:03:46 +0100

    im going crazy

--- AioWebAndroid/settings.gradle.kts
+++ AioWebAndroid/settings.gradle.kts
@@ -11,7 +11,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+        // ❌ DO NOT ADD JITPACK HERE
     }
 }
 
