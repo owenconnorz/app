d2e29a8f20fd1366bf6a66991469c33795dda5af
Owen <96526481+owenconnorz@users.noreply.github.com>
2026-04-28 23:30:10 +0100

    will u work

--- AioWebAndroid/settings.gradle.kts
+++ AioWebAndroid/settings.gradle.kts
@@ -3,14 +3,16 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven("https://jitpack.io")
     }
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
+        maven("https://jitpack.io")
     }
 }
 
