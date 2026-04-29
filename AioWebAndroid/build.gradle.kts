1b5f223780c4e81af9180279f3ec814aeeb41f68
Owen <96526481+owenconnorz@users.noreply.github.com>
2026-04-29 02:28:59 +0100

    Update build.gradle.kts

--- AioWebAndroid/build.gradle.kts
+++ AioWebAndroid/build.gradle.kts
@@ -1,11 +1,11 @@
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:8.3.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
+        classpath("com.android.tools.build:gradle:8.1.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.application") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
