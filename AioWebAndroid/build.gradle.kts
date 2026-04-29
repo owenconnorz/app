db2871d1a9b45a266a2e24dcb9b26e7a268f1b33
Owen <96526481+owenconnorz@users.noreply.github.com>
2026-04-29 12:29:43 +0100

    not going to work

--- AioWebAndroid/build.gradle.kts
+++ AioWebAndroid/build.gradle.kts
@@ -1,11 +1,13 @@
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        // Required for older plugins that still use buildscript
+        classpath("com.android.tools.build:gradle:8.3.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.android.application") version "8.3.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.20" apply false
 }
