9ea69ad9dc1624ab238265da4ffdadc395c36e84
Owen <96526481+owenconnorz@users.noreply.github.com>
2026-05-04 22:03:06 +0100

    fixes

---
+++ AioWebAndroid/app/src/main/java/com/aioweb/app/data/model/MovieMappers.kt
@@ -0,0 +1,14 @@
+package com.aioweb.app.data.model
+
+import com.aioweb.app.data.api.TmdbMovie
+
+fun TmdbMovie.toUiMovie(): UiMovie {
+    return UiMovie(
+            id = id,
+                    title = displayTitle,
+                            posterUrl = posterUrl ?: "",
+                                    backdropUrl = backdropUrl ?: "",
+                                            releaseDate = releaseDate ?: ""
+                                                )
+                                                }
+                                                }
